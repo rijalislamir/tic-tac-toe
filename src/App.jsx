004b081ce8bcae44b9ac9f899d7a0775aa154677
Rijal Islami <rijalislamir@gmail.com>
2022-07-10 16:44:18 +0700

    Stop game after winner found

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaRegCircle } from "react-icons/fa"
 import { FaTimesCircle } from "react-icons/fa"
 import './App.css';
 
 function App() {
   const [turn, setTurn] = useState(1)
+  const [winCombination, setWinCombination] = useState([])
   const [board, setBoard] = useState([
     undefined,
     undefined,
@@ -16,11 +17,39 @@ function App() {
     undefined,
     undefined
   ])
+  const combinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ]
+
+  useEffect(() => {
+    checkAnyCombination()
+  }, [turn])
 
   const setValue = (index) => {
-    if (board[index] === undefined) {
-      setBoard(prevBoard => prevBoard.map((item, i) => item = i === index ? turn : item))
+    if (board[index] === undefined && !winCombination.length) {
+      setBoard(prevBoard => {
+        let newBoard = prevBoard
+        newBoard[index] = turn
+
+        return newBoard
+      })
       setTurn(prevTurn => prevTurn === 1 ? 2 : 1)
+    }
+  }
+
+  const checkAnyCombination = () => {
+    for (const combination of combinations) {
+      if (board[combination[0]] === board[combination[1]] && board[combination[0]] === board[combination[2]] && board[combination[0]] !== undefined) {
+        setWinCombination(combination)
+        break
+      }
     }
   }
 
@@ -28,7 +57,13 @@ function App() {
     <div className='flex flex-col justify-center items-center bg-blue-500 h-screen w-screen'>
       <h1>Tic Tac Toe</h1>
       <div className='flex flex-wrap mx-auto text-center content-between justify-between w-[90vw] max-w-[450px] h-[90vw] max-h-[450px]'>
-        {board.map((item, i) => <div key={i} onClick={() => setValue(i)} className="flex items-center justify-center bg-blue-400 w-[30%] h-[30%] cursor-pointer">
+        {board.map((item, i) => 
+          <div 
+            key={i}
+            onClick={() => setValue(i)}
+            className={winCombination.includes(i)
+              ? "flex items-center justify-center bg-green-400 w-[30%] h-[30%] cursor-pointer"
+              : "flex items-center justify-center bg-blue-400 w-[30%] h-[30%] cursor-pointer"}>
           {item === undefined 
             ? null 
             : item === 1 
